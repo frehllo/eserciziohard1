9be0c8f89053850ee912d43fe4c61e80f46b513f
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-03-04 12:33:49 +0100

    Add project files.

---
+++ esercizio/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Numerics;
+
+namespace Esercizio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int c = 0;
+                int j = 0;
+                int d = 0;
+                string fileinput = "input.txt";
+                string fileoutput = "output.txt";
+                string num = " ";
+                int n;
+                char stop;
+                string numero = "";
+                BigInteger x = new BigInteger();
+                int i = 0;
+
+                if (File.Exists(fileinput))
+                {
+                    using (StreamReader reader = new StreamReader(fileinput))
+                    {
+                        num = reader.ReadLine();
+                    }
+                }
+
+                using (StreamWriter w = new StreamWriter(fileoutput))
+                {
+
+                    do
+                    {
+                        string lunghezza = "";
+                        j = i;
+                        d = 0;
+                        numero = "";
+                        do
+                        {
+                            stop = num[j];
+                            j++;
+                            d++;
+                        } while (stop != '1');
+
+                        for (c = j; c < j + d - 1; c++)
+                        {
+                            lunghezza += int.Parse(num[c].ToString());
+                        }
+
+                        n = int.Parse(lunghezza);
+
+                        for (i = c; i < c + n; i++)
+                        {
+                            numero += num[i ].ToString();
+                        }
+
+                        x = BigInteger.Parse(numero);
+
+                        w.WriteLine(x);
+
+                        Console.WriteLine($"{x}");
+
+                    } while (num[i] != '*');
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
